6f210c9c5aff3787fcb7c9356d6a8322d437d2ea
Aditya Pacharne <48561911+AdityaPacharne@users.noreply.github.com>
2025-02-05 15:45:41 +0530

    switched from linear iteration to go routines

--- game.go
+++ game.go
@@ -7,7 +7,21 @@ import (
 	"os/exec"
 	"strings"
 	"golang.org/x/sys/unix"
+	"time"
+	"sync"
 )
+
+type Rocks struct {
+	existence bool
+	xaxis int
+	yaxis int
+}
+
+type bullet struct {
+	existence bool
+	xaxis int
+	yaxis int
+}
 
 func spaceship (spaceshipPosition *[][]string, currentHeight int, currentWidth int, direction string, terminalHeight int, terminalWidth int) int {
 	if direction == "k" {
@@ -28,20 +42,40 @@ func spaceship (spaceshipPosition *[][]string, currentHeight int, currentWidth i
 	return currentWidth;
 }
 
-func render(screen [][]string) {
-    fmt.Print("\033[H\033[2J") // Clear screen
-    for _, row := range screen {
-        fmt.Println(strings.Join(row, ""))
-    }
+func render(screen *[][]string, quit chan bool) {
+	for {
+		select {
+		case <- quit:
+			return
+		default:
+			fmt.Print("\033[H\033[2J")
+			for _, row := range (*screen) {
+				fmt.Println(strings.Join(row, ""))
+			}
+			time.Sleep(100 * time.Millisecond);
+		}
+	}
 }
 
 func getTerminalSize() (width, height int) {
-	// var ws unix.Winsize
 	ws, err := unix.IoctlGetWinsize(0, unix.TIOCGWINSZ)
 	if err != nil {
 		return 80,24;
 	}
 	return int(ws.Col), int(ws.Row)
+}
+
+func playerInput(screen *[][]string, currentHeight int, currentWidth int, height int, width int, quit chan bool) {
+	reader := bufio.NewReader(os.Stdin);
+	for {
+		input, _ := reader.ReadByte();
+		var inputString string = string(input);
+		if inputString == "q" {
+			quit <- true;
+			break;
+		}
+		currentWidth = spaceship(screen, currentHeight, currentWidth, inputString, height, width);
+	}
 }
 			
 func main() {
@@ -52,7 +86,6 @@ func main() {
 	exec.Command("stty", "-f", "/dev/tty", "cbreak", "min", "1").Run();
 	exec.Command("stty", "-f", "/dev/tty", "-echo").Run();
 
-	reader := bufio.NewReader(os.Stdin);
 
 	var width, height int = getTerminalSize();
 	fmt.Println("Width: ", width);
@@ -68,18 +101,24 @@ func main() {
 	screen[height-1][width/2] = "H";
 	var currentHeight int = height-1;
 	var currentWidth int = width/2;
-	fmt.Println("Spaceship Height: ", currentHeight);
-	fmt.Println("Spaceship Width: ", currentWidth);
 
-	for {
-		render(screen);
-		input, _ := reader.ReadByte();
-		var inputString string = string(input);
-		if inputString == "q" {
-			break;
-		}
-		currentWidth = spaceship(&screen, currentHeight, currentWidth, inputString, height, width);
-	}
+	quit := make(chan bool);
+	
+	var wg sync.WaitGroup
+	wg.Add(2);
+
+	go func() {
+		defer wg.Done();
+		render(&screen, quit);
+	}();
+
+	go func() {
+		defer wg.Done();
+		playerInput(&screen, currentHeight, currentWidth, height, width, quit);
+	}();
+
+	wg.Wait();
+
 	exec.Command("stty", string(player_stty_settings)).Run();
 	fmt.Println("Your stty settings have been restored");
 	fmt.Println("Thanks for playing the game");
